713a64eebbbfc27b9c1c09326f3eb5fdc04cf32a
Ronak Malani <ronakm@gluelabs.com>
2023-09-18 17:24:22 +0530

    2nd leasson

---
+++ lesson-2/script.js
@@ -0,0 +1,11 @@
+function maxNumber(val1, val2) {
+    return (val1 > val2) ? val1 : val2 //ternary operator
+
+    // if (val1 > val2)         
+    //     return val1             // normal syntax
+    // else {
+    //     return val2
+    // }
+}
+
+let maxNumber = maxNumber(300, 400)
